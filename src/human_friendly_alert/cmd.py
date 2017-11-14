58e8bacd6eeec538310984ef9d7a80e74ed38bd3
Kimihiro N <kimihiro.nisimura@gmail.com>
2017-11-14 09:56:40 +0900

    Setup Flask

--- src/human_friendly_alert/cmd.py
+++ src/human_friendly_alert/cmd.py
@@ -1,6 +1,18 @@
 # coding: utf-8
 
+import sys
+from os.path import dirname
+from logging import getLogger, StreamHandler, DEBUG
+
+from flask import Flask
 import click
+
+handler = StreamHandler(sys.stdout)
+logger = getLogger(dirname(__name__))
+logger.propagate = False
+logger.addHandler(handler)
+
+app = Flask(__name__)
 
 
 @click.group()
@@ -10,6 +22,17 @@ def cmd():
 @cmd.command()
 def alert():
     click.echo('Hello world')
+
+@cmd.command()
+@click.option('--debug', is_flag=True, default=True)
+@click.option('--port', type=int, default='8000')
+@click.option('--host', default='0.0.0.0')
+def runserver(host, port, debug):
+    if debug:
+        app.debug = True
+        handler.setLevel(DEBUG)
+        logger.setLevel(DEBUG)
+    app.run(host=host, port=port)
 
 def main():
     cmd()
